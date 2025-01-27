dcb7ffb0502fb83b24ebf78a6836180cce924e9f
X2ET <noeth11@outlook.com>
2025-01-28 02:31:51 +1300

    Update u_ech_test.go

--- u_ech_test.go
+++ u_ech_test.go
@@ -5,8 +5,8 @@ import (
 	"io"
 	"testing"
 
-	tls "github.com/refraction-networking/utls"
-	"github.com/refraction-networking/utls/dicttls"
+	tls "github.com/uQUIC/utls"
+	"github.com/uQUIC/utls/dicttls"
 )
 
 func TestGREASEECHWrite(t *testing.T) {
